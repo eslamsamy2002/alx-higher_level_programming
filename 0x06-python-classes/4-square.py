7bd95127a10c705503a24e47e6566414814a2c22
eslam <eslam@org.com>
2023-10-24 12:33:29 -0400

    mm

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """defines square"""
+
+    def __init__(self, size=0):
+        """init
+        Args:
+            size: length
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """property for the length
+
+        raises:
+            type error: if size not integer
+            value error: if size < 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >=0')
+        self.__size = value
+
+    def area(self):
+        """area of a square"""
+        return self.__size * self.__size
