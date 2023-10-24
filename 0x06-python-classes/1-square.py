83993097f34be3df9ab8c90735fbd96b0014037b
eslam <eslam@org.com>
2023-10-24 11:58:03 -0400

    mm

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """define square"""
+
+    def __init__(self, size):
+        """init
+        Args:
+            size: ..
+        """
+        self.__size = size
