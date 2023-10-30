a0e46f1e855913db0d6b04d76ff528f31ae24eac
eslam <eslam@org.com>
2023-10-30 17:38:20 -0400

    mm

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -128,7 +128,6 @@ def recursive_solve(board, row, queens, solutions):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         sys.exit(1)
