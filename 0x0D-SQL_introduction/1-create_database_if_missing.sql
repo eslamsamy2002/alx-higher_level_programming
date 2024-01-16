f48a7d91a145fc489ac28d1a25bcb95709fbb3df
eslam <eslam@org.com>
2024-01-16 10:55:28 -0500

    mm

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- a script that creates the database hbtn_0c_0 in your MySQL server
-CREATE DATABASE IF NOT EXISTS 'htbn_0c_0';
+-- Creat Database if not exists
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
