9463b9a10baa13d99bacd357ff1f1529d6aecdfc
eslam <eslam@org.com>
2024-03-13 09:37:37 -0400

    mm

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,40 @@ class Square(Rectangle):
         """string"""
         return f"[Square] ({self.id}) {self.x}/{self.y} " \
             f"- {self.height}"
+    
+    @property
+    def size(self):
+        """getter"""
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        self.width = value
+        self.height = self.width
+    
+    def update(self, *args, **kwargs):
+        """update"""
+
+        if args:
+
+            att_list = ["id", "size", "x", "y"]
+            for i , value in enumerate(args):
+                setattr(self, att_list[i], value)
+            else:
+                for key, value in kwargs.items():
+                    if hasattr(self, key):
+                        setattr(self, key, value)
+                    else:
+                        raise ValueError(f"{key} is not attribute in this class")
+
+
+
+
+
+
+
+
+
+
