51cd365924947a00890eaed7f9d679342c8158ef
eslam <eslam@org.com>
2024-03-14 11:36:14 -0400

    mm

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """base class module"""
+
+import json
+
 class Base:
     """Base Class"""
 
@@ -15,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+        @staticmethod
+    def to_json_string(list_dictionaries):
+        """dictionaries to json"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
