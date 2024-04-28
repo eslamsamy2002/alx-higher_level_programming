69d98c9d27b2861fd7d833bdbebef692ae797c03
eslam <eslam@org.com>
2024-04-28 16:59:44 -0400

    mm

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,11 @@
--- Check if the user exists
-SELECT COUNT(*) INTO @user_exists FROM mysql.user WHERE user = 'user_0d_1';
+-- add user GRANT SELECT
 
--- If the user does not exist, create it
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-END IF;
 
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
