8062f5e1aea5566428d163fabaee926584f9dcf9
eslam <eslam@org.com>
2024-05-01 07:39:56 -0400

    mm

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,12 +3,12 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE 
-	IF NOT EXISTS states
-		(
-			id INT
-			   PRIMARY KEY
-			   NOT NULL 
-			   AUTO_INCREMENT,
-			name VARCHAR(256)
-			     NOT NULL
-		);
+	IF NOT EXISTS hbtn_0d_usa.states
+	(
+		id 	INT
+			PRIMARY KEY
+			NOT NULL
+			AUTO_INCREMENT,
+		name VARCHAR(256)
+			 NOT NULL
+	);
